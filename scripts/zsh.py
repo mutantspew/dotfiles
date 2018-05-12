576dfc4e49b33968b97dfc1f7b687186aa786089
Jacob Joslin <mutantspew@gmail.com>
2018-05-12 00:31:04 -0400

    Added debugging messages

--- scripts/zsh.py
+++ scripts/zsh.py
@@ -38,20 +38,24 @@ class ZSH(object):
     self.link_zsh_config()
 
   def check_zsh_installed(self):
+    self.log.print('Checking for Zsh', ll.Debug)
     return shutil.which('zsh')
 
   def check_oh_my_zsh_installed(self):
     pass
 
   def install_zsh(self):
+    self.log.print('Installing Zsh', ll.Debug)
     try:
       ret = subprocess.run(['sudo', 'apt-get', 'install', 'zsh'], check = True)
     except subprocess.CalledProcessError as err:
       self.log.print("Error: {}".format(err), ll.Error) # an error occurred
     else:
+      self.log.print('Zsh installed', ll.Debug)
       return True
 
   def set_zsh_default(self):
+    self.log.print('Setting Zsh as the default shell', ll.Debug)
     try:
       ret = subprocess.run(['chsh', 'zsh'], check = True)
     except subprocess.CalledProcessError as err:
@@ -60,6 +64,7 @@ class ZSH(object):
       return True
 
   def install_oh_my_Zsh(self):
+    self.log.print('Installing Oh My Zsh', ll.Debug)
     args = ['sh', '-c', "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"]
 
     try:
