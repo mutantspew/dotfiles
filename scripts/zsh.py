97982de7809ad537457aa53253eac4749e46f5dc
Jacob Joslin <mutantspew@gmail.com>
2018-05-12 00:28:06 -0400

    check-zsh-installed, install zsh, set-zsh-default, and install-oh-my-zsh

--- scripts/zsh.py
+++ scripts/zsh.py
@@ -1,5 +1,8 @@
 import os, shutil, subprocess, sys
 
+import log
+from log import LogLevel as ll
+
 import zsh, backup
 
 class ZSH(object):
@@ -10,22 +13,61 @@ class ZSH(object):
     self.config = zshconfig
 
   def run(self):
-    pass
+    l = self.log
+
+    l.print("Checking if zsh is installed", ll.Debug)
+    if(self.check_zsh_installed() is False):
+      l.print("Not installed", ll.Debug)
+      choice = input('Install Zsh? [y/n]').lower()
+
+      if(choice == 'y'):
+        self.install_zsh()
+      else: # nothing left to do
+        return
+
+      # set zsh as the default
+     self.self.set_zsh_default()
+
+      if(self.check_oh_my_zsh_installed() is False):
+        self.install_oh_my_Zsh()
+
+    # backup zsh default configs
+    self.backup_zsh_config()
+
+    # link files
+    self.link_zsh_config()
 
   def check_zsh_installed(self):
-    pass
+    return shutil.which('zsh')
 
   def check_oh_my_zsh_installed(self):
     pass
 
   def install_zsh(self):
-    pass
+    try:
+      ret = subprocess.run(['sudo', 'apt-get', 'install', 'zsh'], check = True)
+    except subprocess.CalledProcessError as err:
+      self.log.print("Error: {}".format(err), ll.Error) # an error occurred
+    else:
+      return True
 
   def set_zsh_default(self):
-    pass
+    try:
+      ret = subprocess.run(['chsh', 'zsh'], check = True)
+    except subprocess.CalledProcessError as err:
+      self.log.print("Error: {}".format(err), ll.Error) # an error occurred
+    else:
+      return True
 
   def install_oh_my_Zsh(self):
-    pass
+    args = ['sh', '-c', "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"]
+
+    try:
+      ret = subprocess.run(args, check = True)
+    except subprocess.CalledProcessError as err:
+      self.log.print("Error: {}".format(err), ll.Error) # an error occurred
+    else:
+      return True
 
   def backup_zsh_config(self):
     pass
